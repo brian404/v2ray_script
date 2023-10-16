9fc597d3a96cabeb36d09b61131c914ec87673f5
brian <72920128+brian404@users.noreply.github.com>
2023-10-16 10:20:50 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,41 +1,79 @@
 #!/bin/bash
 
-# Replace DOMAIN with your actual domain name
-DOMAIN="nginx.brian72.eu.org"
+# Configure iptables
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -F
+sudo cp /etc/iptables/rules.v4 /etc/iptables/rules.v4.bak
+sudo truncate -s 0 /etc/iptables/rules.v4
 
-# Install Nginx and V2Ray
-sudo apt update
-sudo apt install -y nginx
-sudo apt install -y wget unzip
-bash <(curl -L -s https://install.direct/go.sh)
+# Prompt user for domain name
+read -p "Enter domain name: " domain_name
 
-# Configure Nginx to serve as a reverse proxy for V2Ray
-sudo tee /etc/nginx/sites-available/$DOMAIN >/dev/null <<EOF
+# Prompt user for panel username
+read -p "Enter panel username: " username
+
+# Prompt user for panel password
+read -p "Enter panel password: " password
+
+# Prompt user for panel port
+read -p "Enter panel port: " port
+
+# Update and upgrade system
+apt update && apt upgrade -y
+
+# Install required packages
+apt install curl socat -y
+
+# Install acme.sh
+curl https://get.acme.sh | sh
+
+# Set default CA server
+~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+
+# Register account
+~/.acme.sh/acme.sh --register-account -m vicpku1@gmail.com
+
+# Issue certificate
+~/.acme.sh/acme.sh --issue -d $domain_name --standalone
+
+# Install certificate
+~/.acme.sh/acme.sh --installcert -d $domain_name --key-file /root/private.key --fullchain-file /root/cert.crt
+
+# Install Nginx
+apt install nginx -y
+
+# Configure Nginx
+cat > /etc/nginx/sites-available/mypanel <<EOL
 server {
     listen 80;
-    listen [::]:80;
-    server_name $DOMAIN;
-    return 301 https://$DOMAIN$request_uri;
-}
-
-server {
-    listen 443 ssl http2;
-    listen [::]:443 ssl http2;
-    server_name $DOMAIN;
-
-    ssl_certificate /etc/v2ray/v2ray.crt;
-    ssl_certificate_key /etc/v2ray/v2ray.key;
-    ssl_protocols TLSv1.2 TLSv1.3;
+    server_name $domain_name;
 
     location / {
-        proxy_pass http://127.0.0.1:10000;
+        proxy_pass http://127.0.0.1:$port;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
+        proxy_set_header Host \$host;
     }
 }
-EOF
+EOL
 
-# Enable the Nginx configuration and restart Nginx
-sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
-sudo systemctl restart nginx
+# Create a symbolic link to enable the site
+ln -s /etc/nginx/sites-available/mypanel /etc/nginx/sites-enabled/
+
+# Remove the default Nginx site
+rm /etc/nginx/sites-enabled/default
+
+# Test Nginx configuration
+nginx -t
+
+# Reload Nginx
+systemctl reload nginx
+
+# Run x-ui install script with user prompts
+echo -e "y\n$username\n$password\n$port" | bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+
+# Print server access information
+echo -e "\033[32mVisit http://$domain_name to access the panel.\033[0m"
